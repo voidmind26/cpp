42b951fb84010a421c01dfb268d236b03327de33
voidmind26 <2805721993@qq.com>
2023-08-04 14:34:10 +0800

    modified:   luogu/p1434_dp.cpp

--- luogu/p1434_dp.cpp
+++ luogu/p1434_dp.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
-const int N=
+const int N = 101;
+int a[N][N];
+int dp[N][N];
 
 int main() {
-
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1;i <= n;i++) {
+        for (int j = 1;j <= m;j++) {
+            cin >> a[i][j];
+        }
+    }
     return 0;
 }
